110c6627429ffa1e5ab3126464f4223a67d8c50a
alan bensi <alanbensi9@gmail.com>
2022-07-04 16:46:02 -0300

    Avances en las rutas. Solo estan armadas las rutas principales, despues se van a ir agregando mas especificas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Inicio from './components/Inicio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path='/' element= {<Inicio />} />
+        {/* <Route exact path='/productos' element= {<Productos />} /> */}
+        {/* <Route exact path='/subastas' element= {<Subastas />} /> */}
+        {/* <Route exact path='/vendeTuCamiseta' element= {<VendeTuCamiseta />} /> */}
+        {/* <Route exact path='/nosotros' element= {<Nosotros />} /> */}
+        {/* <Route exact path='/ayuda' element= {<Ayuda />} /> */}
+        {/* <Route exact path='/carrito' element= {<Carrito />} /> */}
+      </Routes>
     </div>
   );
 }
