85b70b08664cd0c95852afc31431cc1629d9470a
alan bensi <alanbensi9@gmail.com>
2022-09-28 17:45:51 -0300

    Pagina detalle subastas lista

--- src/components/pages/detalleSubasta/DetalleSubastas.jsx
+++ src/components/pages/detalleSubasta/DetalleSubastas.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './DetalleSubastas.css';
-import { useCountdown } from '../../../hooks/useCountdown'
+import CardsSubastas from '../../atoms/cardsSubastas/CardsSubastas';
+import Boton from '../../atoms/boton/Boton'
+import { Button, Modal } from 'react-bootstrap';
 
-const DetalleSubastas = ({camiseta}) => {
-    const date2 = "22/10/2022"
-    const time2 = "00:00"
-
-    const {timeCounter ,daysLeft, hoursLeft, minutesLeft, secondsLeft} = useCountdown(date2, time2);
+const DetalleSubastas = () => {
     const ruta = useLocation();
     const [titulo, setTitulo] = useState("");
 
@@ -16,9 +14,11 @@ const DetalleSubastas = ({camiseta}) => {
         setTitulo(sinEspacios.replace("/", " "));
     }, [ruta]);
 
-    setInterval(() => {
-        timeCounter()
-     }, 5000)
+    const ofertaUsuarioEsMaxima = true;
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
         <main className='px-3'>
@@ -29,7 +29,52 @@ const DetalleSubastas = ({camiseta}) => {
             </section>
             <section>
                 <h1 className='tituloDetalleSubastas'>Camiseta Lorem ipsum, dolor sit amet</h1>
-                <h3 className=''>Termina en <span>{daysLeft} dias </span><br/><span>{hoursLeft} hr </span><span>{minutesLeft} min</span> <span>{secondsLeft} seg</span></h3>  
+                <CardsSubastas imgSubasta='adfsdf' />
+                {ofertaUsuarioEsMaxima && <h3 className='pb-4'>TENÉS LA OFERTA MÁS ALTA</h3>}
+                <div>
+                    <Button className='verOfertasSubastas' onClick={handleShow}>
+                        VER TODAS LAS OFERTAS
+                    </Button>
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton />
+                        <Modal.Body>ACA VA EL LISTADO DE LAS OFERTAS PREVIAS.</Modal.Body>
+                    </Modal>
+                </div>
+                <div className='mt-2'>
+                    <p>Siguiente oferta mínima: USD "OFERTA * 0.10"</p>
+                </div>
+                <div>
+                    <Link className='verOfertasSubastas estiloLinks' to="/">HACER UNA OFERTA</Link>
+                </div>
+                <div className='containerDescripcionSubastas'>
+                    <h2 className='subtituloDetalleSubastas'>DESCRIPCIÓN</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut maiores commodi fugit nulla, voluptatibus inventore obcaecati numquam incidunt provident officia veritatis voluptates earum voluptatum itaque minima aliquid hic amet sint.</p>
+                    <div>
+                        <table className='tablaDescripcionSubasta'>
+                            <tr>
+                                <td>EQUIPO</td>
+                                <td>Info dinamica</td>                                  
+                            </tr>
+                            <tr>
+                                <td>OBJETO</td>
+                                <td>Info dinamica</td>
+                            </tr>
+                            <tr>
+                                <td>DEPORTISTA</td>
+                                <td>Info dinamica</td>
+                            </tr>
+                            <tr>
+                                <td>ESTADO</td>
+                                <td>Info dinamica</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div className='containerBotonHacerOferta'>
+                    <Link className='estiloLinks' to="/">
+                        <Boton texto="Hacer una oferta" estilo="botonAzul botonLogin" />
+                    </Link>
+                </div>
             </section>
         </main>
     )
--- src/components/pages/inicio/Inicio.jsx
+++ src/components/pages/inicio/Inicio.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Banner from '../../atoms/banner/Banner'
 import Cards from '../../atoms/cards/Cards'
 import {Container, Row, Col} from 'react-bootstrap'
@@ -6,36 +6,49 @@ import './Inicio.css'
 import { Icon } from '@iconify/react'
 import Slider from '../../moleculs/slider/Slider'
 import { Link } from 'react-router-dom';
+import LoadingSpinner from '../../atoms/loading/LoadingSpinner'
+
 
 const Inicio = () => {
+
+    const [cargando, setCargando] = useState(false)
+
+
     return (
         <div>
-            <Banner />
-            <h1 className='tituloInicio'>BIENVENIDOS A CLÁSICA 1905</h1>
-            <Slider />
-            <div className='contenedorEnviosGratis'>
-                <Icon icon="carbon:delivery" width='32px' color='white' />
-                <p className='textoEnviosGratis'>Envios gratis a todo el pais</p>
-            </div>
-            <h2 className='titOfertasInicio'>OFERTAS</h2>
-            <Container>
-                <Row>
-                    <Col lg ={3} md = {6} xs = {6}>
-                        <Link to="/Detalle-Camisetas" className='estiloLinks'>
-                            <Cards img = 'adfsdf' titulo = 'Camiseta 1' precio ='$10000.00' precioAntiguo= '$20000'/>
-                        </Link>
-                    </Col>
-                    <Col lg ={3} md = {6} xs = {6}>
-                        <Cards img='adfsdf' titulo='Camiseta 1' precio='$10000.00' precioAntiguo='$20000'/>
-                    </Col>
-                    <Col lg ={3} md = {6} xs = {6}>
-                        <Cards img='adfsdf' titulo='Camiseta 1' precio='$10000.00' precioAntiguo='$20000'/>
-                    </Col>
-                    <Col lg ={3} md = {6} xs = {6}>
-                        <Cards img='adfsdf' titulo='Camiseta 1' precio='$10000.00' precioAntiguo='$20000'/>
-                    </Col>
-                </Row>
-            </Container>
+            {cargando ? 
+                (<LoadingSpinner/>) 
+            : 
+                (<>
+                    <Banner />
+                    <h1 className='tituloInicio'>BIENVENIDOS A CLÁSICA 1905</h1>
+                    <Slider />
+                    <div className='contenedorEnviosGratis'>
+                        <Icon icon="carbon:delivery" width='32px' color='white' />
+                        <p className='textoEnviosGratis'>Envios gratis a todo el pais</p>
+                    </div>
+                    <h2 className='titOfertasInicio'>OFERTAS</h2>
+                    <Container>
+                        <Row>
+                            <Col lg={3} md={6} xs={6}>
+                                <Link to="/Detalle-Camisetas" className='estiloLinks'>
+                                    <Cards img='adfsdf' titulo='Camiseta 1' precio='$10000.00' precioAntiguo='$20000' />
+                                </Link>
+                            </Col>
+                            <Col lg={3} md={6} xs={6}>
+                                <Cards img='adfsdf' titulo='Camiseta 1' precio='$10000.00' precioAntiguo='$20000' />
+                            </Col>
+                            <Col lg={3} md={6} xs={6}>
+                                <Cards img='adfsdf' titulo='Camiseta 1' precio='$10000.00' precioAntiguo='$20000' />
+                            </Col>
+                            <Col lg={3} md={6} xs={6}>
+                                <Cards img='adfsdf' titulo='Camiseta 1' precio='$10000.00' precioAntiguo='$20000' />
+                            </Col>
+                        </Row>
+                    </Container>
+                </>
+                )
+            }
         </div>
     )
 }
