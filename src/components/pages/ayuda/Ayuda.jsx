60ed4970819e6c66e6fab6be6db70ec859f8d491
alan bensi <alanbensi9@gmail.com>
2022-10-07 19:45:16 -0300

    Pagina ayuda lista en desktop

--- src/components/pages/ayuda/Ayuda.jsx
+++ src/components/pages/ayuda/Ayuda.jsx
@@ -17,32 +17,25 @@ const Ayuda = () => {
 
     return (
         <>
-            <main className='px-3'>
+            <main className='mainAyudaDesktop'>
                 <section className='d-flex mt-2'>
                     <Link className='breadcrumb' to='/'>Inicio {'>'}</Link>
                     <h1 className='ms-1 tituloAyuda'>{titulo}</h1>
                 </section>
-                <section className='d-flex'>
-                    <div className='containerAyudaBuscar w-75'>
-                        <input type="search" placeholder='Buscar en ayuda...' className='inputAyudaBuscar ' />
-                        <Icon icon="akar-icons:search" className='iconoAyudaBuscar' />
-                    </div>
-                    <Boton estilo = 'botonAzul botonBusquedaAyuda' texto = 'Buscar' />
-                </section>
                 <section>
-                    <div>
+                    <div className='contenedorCategoriasFaqs'>
                         <h2 className='tituloCategoriasFaqs'>COMPRAS</h2>
                         <Acordeon keyPregunta="1" pregunta="Pregunta número 1" respuesta= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quae fugit temporibus laborum, quod deserunt sed at, amet, vel sequi porro dolores. Velit eum itaque deserunt molestias quisquam, nemo temporibus!" />
                         <Acordeon keyPregunta="2" pregunta="Pregunta número 2" respuesta="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quae fugit temporibus laborum, quod deserunt sed at, amet, vel sequi porro dolores. Velit eum itaque deserunt molestias quisquam, nemo temporibus!" />
                         <Acordeon keyPregunta="3" pregunta="Pregunta número 3" respuesta="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quae fugit temporibus laborum, quod deserunt sed at, amet, vel sequi porro dolores. Velit eum itaque deserunt molestias quisquam, nemo temporibus!" />
                     </div>
-                    <div>
+                    <div className='contenedorCategoriasFaqs'>
                         <h2 className='tituloCategoriasFaqs'>SUBASTAS</h2>
                         <Acordeon keyPregunta="1" pregunta="Pregunta número 1" respuesta="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quae fugit temporibus laborum, quod deserunt sed at, amet, vel sequi porro dolores. Velit eum itaque deserunt molestias quisquam, nemo temporibus!" />
                         <Acordeon keyPregunta="2" pregunta="Pregunta número 2" respuesta="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quae fugit temporibus laborum, quod deserunt sed at, amet, vel sequi porro dolores. Velit eum itaque deserunt molestias quisquam, nemo temporibus!" />
                         <Acordeon keyPregunta="3" pregunta="Pregunta número 3" respuesta="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quae fugit temporibus laborum, quod deserunt sed at, amet, vel sequi porro dolores. Velit eum itaque deserunt molestias quisquam, nemo temporibus!" />
                     </div>
-                    <div>
+                    <div className='contenedorCategoriasFaqs'>
                         <h2 className='tituloCategoriasFaqs'>ENVIOS</h2>
                         <Acordeon keyPregunta="1" pregunta="Pregunta número 1" respuesta="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quae fugit temporibus laborum, quod deserunt sed at, amet, vel sequi porro dolores. Velit eum itaque deserunt molestias quisquam, nemo temporibus!" />
                         <Acordeon keyPregunta="2" pregunta="Pregunta número 2" respuesta="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quae fugit temporibus laborum, quod deserunt sed at, amet, vel sequi porro dolores. Velit eum itaque deserunt molestias quisquam, nemo temporibus!" />
