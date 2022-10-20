238154bde56c565cf6ea506508c4f182e49e5048
Sofia Carena <sofiacarena97@gmail.com>
2022-10-20 01:56:29 -0300

    completes users crud and username deletion

--- server/db/user_admin.js
+++ server/db/user_admin.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 const sequelize = require('../server.js');
 
 // Creates Admin user 1
-sequelize.query('INSERT INTO users (username, fullname, email, password, address, phone, admin) VALUES (?, ?, ?, ?, ?, ?, ?)',
-    {replacements: ['SuperAdmin', 'Super Admin', 'superadmin@gmail.com', 'clasica123', 'calle falsa 123', '11111111', true]}
+sequelize.query('INSERT INTO users (name, surname, fullname, email, password, phone, status, admin) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+    {replacements: ['Super', 'Admin', 'Super Admin', 'superadmin@gmail.com', 'clasica123', '11111111', true, true]}
 ).then((response) => {
     console.log("SuperAdmin user created succesfully");
 }).catch((err) => {
@@ -11,8 +11,8 @@ sequelize.query('INSERT INTO users (username, fullname, email, password, address
 });
 
 // Creates Admin user 2
-sequelize.query('INSERT INTO users (username, fullname, email, password, address, phone, admin) VALUES (?, ?, ?, ?, ?, ?, ?)',
-    {replacements: ['SuperAdmin2', 'Super Admin', 'superadmin2@gmail.com', 'clasica123', 'calle falsa 123', '11111111', true]}
+sequelize.query('INSERT INTO users (name, surname, fullname, email, password, phone, status, admin) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+    {replacements: ['Super', 'Admin', 'Super Admin', 'superadmin2@gmail.com', 'clasica123', '11111111', true, true]}
 ).then((response) => {
     console.log("SuperAdmin2 user created succesfully");
 }).catch((err) => {
