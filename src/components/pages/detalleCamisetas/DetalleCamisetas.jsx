5517d900594ee156da954b0ef82453c2d6788412
alan bensi <alanbensi9@gmail.com>
2023-01-03 16:25:53 -0300

    Cambio valor precio segun swipe listo en home y colecciones. (Falta visualizar el valor en dolares por problema con prop)

--- src/components/pages/detalleCamisetas/DetalleCamisetas.jsx
+++ src/components/pages/detalleCamisetas/DetalleCamisetas.jsx
@@ -23,8 +23,6 @@ const DetalleCamisetas = () => {
     const switchDivisa = useContext(SwitchDivisaContext);
     const { switchDivisaContexto } = switchDivisa; 
 
-    console.log (switchDivisaContexto, "switch en detalle")
-
     const ruta = useLocation();
     const [camiseta, setCamiseta] = useState({});
     const [id, setId] = useState(0);
