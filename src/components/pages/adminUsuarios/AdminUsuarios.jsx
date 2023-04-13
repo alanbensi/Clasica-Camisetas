4feb13d39a2ebdeb2f1f5305065156b0122198f6
Alan <alanbensi9@gmail.com>
2023-04-13 16:52:02 -0300

    Tabla de usuarios con ordenamientos lista en vista admin

--- src/components/pages/adminUsuarios/AdminUsuarios.jsx
+++ src/components/pages/adminUsuarios/AdminUsuarios.jsx
@@ -2,8 +2,9 @@ import React, { useContext, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import { useFetchData } from '../../../hooks/useFetch';
 import { UserContext } from '../../context/UserContext';
-import { Icon } from '@iconify/react';import { Link } from 'react-router-dom';
-;
+import { Icon } from '@iconify/react';
+import { Link } from 'react-router-dom';
+import { DataGrid } from '@mui/x-data-grid';
 
 const AdminUsuarios = () => {
     const userContext = useContext(UserContext);
@@ -15,41 +16,55 @@ const AdminUsuarios = () => {
         fetchData();
     }, [])
 
+    const columns = [
+        { field: 'name', headerName: 'Nombre', width: 130 },
+        { field: 'surname', headerName: 'Apellido', width: 130 },
+        { field: 'email', headerName: 'Mail', width: 130 },
+        { field: 'verMas', headerName: 'Ver más', width: 130 },
+    ];
+
     return (
         <main>
             {
                 userAdmin ?
                     (
-                        <Table responsive striped bordered hover>
-                            <thead>
-                                <tr>
-                                    <th>Nombre</th>
-                                    <th>Apellido</th>
-                                    <th>Email</th>
-                                    <th>Ver mas</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    data.map ((usuario)=> (
-                                        <tr key={usuario.id}>
-                                            <td>{usuario.name}</td>
-                                            <td>{usuario.surname}</td>
-                                            <td>{usuario.email}</td>
-                                            <td>
-                                                <Link to={`/admin/usuarios/${usuario.id}`}>
-                                                    <Icon icon="bi:eye-fill" />
-                                                </Link>
-                                            </td>
-                                        </tr>
-                                    ))}
-                            </tbody>
-                        </Table>
+                        <DataGrid 
+                        rows={data}
+                        columns={columns}
+                        pageSize={2}
+                        rowsPerPageOptions={[5]}
+                        checkboxSelection
+                        autoHeight
+                        />
+                        // <Table responsive striped bordered hover>
+                        //     <thead>
+                        //         <tr>
+                        //             <th>Nombre</th>
+                        //             <th>Apellido</th>
+                        //             <th>Email</th>
+                        //             <th>Ver mas</th>
+                        //         </tr>
+                        //     </thead>
+                        //     <tbody>
+                        //         {
+                        //             data.map ((usuario)=> (
+                        //                 <tr key={usuario.id}>
+                        //                     <td>{usuario.name}</td>
+                        //                     <td>{usuario.surname}</td>
+                        //                     <td>{usuario.email}</td>
+                        //                     <td>
+                        //                         <Link to={`/admin/usuarios/${usuario.id}`}>
+                        //                             <Icon icon="bi:eye-fill" />
+                        //                         </Link>
+                        //                     </td>
+                        //                 </tr>
+                        //             ))}
+                        //     </tbody>
+                        // </Table>
                     )
                     :
                     (<p>Hola</p>)
             }
-
         </main>
     );
 }
